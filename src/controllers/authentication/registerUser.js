b9a6d0e0fbc61509998c3cfbc954d9798178e058
Alia Moussa <atfathi20@gmail.com>
2023-11-25 13:23:22 +0200

    Added Email validation for registerUser

--- src/controllers/authentication/registerUser.js
+++ src/controllers/authentication/registerUser.js
@@ -4,22 +4,32 @@ const bcrypt = require("bcrypt");
 
 const registerUser = async (req, res) => {
   const body = req.body;
-  const newUser = new User({
-    name: body.name,
-    email: body.email,
-    phoneNumber: body.phoneNumber,
-    password: body.password,
-  });
 
-  bcrypt.hash(
-    newUser.password + process.env.PEPPER,
-    +process.env.SALT_ROUNDS || "",
-    async (err, hash) => {
-      newUser.password = hash;
-      await newUser.save();
-      res.send("New User Saved");
+  // validate using email so that for each email, there is only one Intellifix account
+  User.findOne({ email: body.email }).then((user) => {
+    if (user) {
+      res.send("Email Exists");
+    } else {
+      const newUser = new User({
+        name: body.name,
+        email: body.email,
+        phoneNumber: body.phoneNumber,
+        password: body.password,
+    
+      });
+    
+      bcrypt.hash(
+        newUser.password + process.env.PEPPER,
+        +process.env.SALT_ROUNDS || "",
+        async (err, hash) => {
+          newUser.password = hash;
+          await newUser.save();
+          res.send("New User Saved");
+        }
+      );
     }
-  );
-};
+
+  });
+}
 
 module.exports = registerUser;
