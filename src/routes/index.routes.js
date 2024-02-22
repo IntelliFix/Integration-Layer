9b25fffcfd5f0d29f1ce7bb527164e4d78008977
Mohammed Abdelmegeed <m.maguid9@hotmail.com>
2024-02-22 14:04:04 +0200

    Connected IntegrationLlayer and Gen AI Server

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const registerUser = require("../controllers/authentication/registerUser");
+const codeFixer = require("../controllers/security/codeFixer");
+const chatbot = require("../controllers/security/chatbot");
 
 const router = express.Router();
 router.post("/register-user", registerUser);
+// Both code-fixer and chatbot routes have to be protected/authenticated routes
+router.post("/code-fixer", codeFixer);
+router.post("/chatbot", chatbot);
 router.get("/", (req, res) => {
   res.send("Coming Sooon..");
 });
