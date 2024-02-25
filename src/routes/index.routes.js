9baef7504883474f3afcd999cd829cd246f9b60a
Alia Moussa <atfathi20@gmail.com>
2024-02-25 19:40:07 +0200

    Signup with handling errors, jwt and cookies complete

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -5,7 +5,6 @@ const chatbot = require("../controllers/security/chatbot");
 
 const authController = require('../controllers/authentication/authController');
 
-
 const router = express.Router();
 
 router.get("/signup", authController.signup_get);
@@ -16,7 +15,6 @@ router.post("/login", authController.login_post);
 
 
 router.post("/register-user", registerUser);
-
 // Both code-fixer and chatbot routes have to be protected/authenticated routes
 router.post("/code-fixer", codeFixer);
 router.post("/chatbot", chatbot);
