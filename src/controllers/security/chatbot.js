7f2ea46927e88805d06b62f829654fd331592761
Mohammed Abdelmegeed <m.maguid9@hotmail.com>
2024-06-13 23:24:25 +0300

    Updated prompt injection model response

--- src/controllers/security/chatbot.js
+++ src/controllers/security/chatbot.js
@@ -1,8 +1,7 @@
 require("dotenv").config();
 const axios = require("axios");
 const genAIServerUrl = require("../../../GenAI.url");
-const promptinjection = require('./promptInjection');
-
+const promptinjection = require("./promptInjection");
 
 const chatbot = async (req, res) => {
   try {
@@ -13,10 +12,8 @@ const chatbot = async (req, res) => {
     const promptContent = promptResponse.predictions[0].content.trim();
     console.log(promptContent);
 
-
     // Check if promptResponse starts with "Yes it is safe to answer"
-    if (promptContent.startsWith("Yes, it is safe to answer")){
-      
+    if (promptContent.startsWith("Yes, it is safe to answer")) {
       //Send the request to the AI server where the main Chatbot model exists
       const response = await axios.post(genAIServerUrl + "/chatbot", {
         // Session id el mafrood negebha men el user logged in, + howa fe chat raqam kam
@@ -29,18 +26,19 @@ const chatbot = async (req, res) => {
       // console.log(req.body);
 
       res.status(200).json({ success: true, data: response.data });
-
-    
-    }else if (promptContent.startsWith("No, it is not safe to answer")){
-      
+    } else if (promptContent.startsWith("No, it is not safe to answer")) {
       //Reply with Prompt_Injection, finetuned model's reponse
-      res.status(200).json({ success: true, data: promptResponse.predictions[0].content });
-
-    }
-    else {
+      console.log(promptResponse);
+      res
+        .status(200)
+        .json({
+          success: true,
+          data: { output: promptResponse.predictions[0].content },
+        });
+    } else {
       // Handle other cases if needed
       // For example, if the promptResponse does not start with either expected string
-            //Send the request to the AI server where the main Chatbot model exists
+      //Send the request to the AI server where the main Chatbot model exists
       const response = await axios.post(genAIServerUrl + "/chatbot", {
         // Session id el mafrood negebha men el user logged in, + howa fe chat raqam kam
         // For now hane3taha men postman 3ady bas ba3d keda hatet7at hena
@@ -53,8 +51,6 @@ const chatbot = async (req, res) => {
 
       res.status(200).json({ success: true, data: response.data });
     }
-    
-    
   } catch (error) {
     console.error("Error:", error);
     res.status(500).json({ success: false, error: error.message });
